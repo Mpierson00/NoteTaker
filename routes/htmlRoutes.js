62d6757a9a7678ee5056cd59a195b0120445b5db
Michael <mpierson00@gmail.com>
2024-04-20 19:54:51 -0400

    added comments on htmlroutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,12 @@
 const path = require('path');
 
 module.exports = (app) => {
+    //Route to get the notes HTML page
     app.get('/notes', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/notes.html'));
     });
 
+    // Default route to index HTML page
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/index.html'));
     });
