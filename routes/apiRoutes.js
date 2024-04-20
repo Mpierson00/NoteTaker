6a29b14e4fd4a9f2bfb401f5470e28c2f0bc143b
Michael <mpierson00@gmail.com>
2024-04-20 19:34:11 -0400

    updated apiroutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,5 +6,16 @@ module.exports = (app) => {
     app.get('/api/notes', (req, res) => {
         res.json(notes);
     });
-    
-}
+
+    app.post('/api/notes', (req, res) => {
+        const newNote = { ...req.body, id: Math.random().toString() };
+        notes.push(newNote);
+        fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(notes));
+        res.json(newNote);
+    });
+
+    app.delete('/api/notes/:id', (req, res) => {
+        const updatedNotes = notes.filter(note => note.id !== req.params.id);
+        fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(updatedNotes))
+    });
+};
