5066ad8c5e00f65eee921a5a51916dfa772e138b
Michael <mpierson00@gmail.com>
2024-04-20 19:44:52 -0400

    added importing of routes and some comments

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//Middleware for parsing JSON
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+//Importing routes
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+//Start the server
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
